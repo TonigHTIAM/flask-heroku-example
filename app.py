f642c0bad8cae86daa268a6a3fb6bf07c50eab3f
TonigHTIAM <51397419+TonigHTIAM@users.noreply.github.com>
2019-06-12 13:14:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-  #    json_data = {'Hello': 'World!'}
-  #    return jsonify(json_data)
-     return"pleng"
+  #  json_data = {'Hello': 'World!'}
+  #  return jsonify(json_data)
+   return"pleng"
 
 
 if __name__ == '__main__':
